0c03e5ce5e0cb6c5c5de884e6c2dedfc55a947e7
RochmatMulyo <127836890+RochmatMulyo@users.noreply.github.com>
2023-11-19 21:19:57 +0700

    CRUD IONIC Angular

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -13,8 +15,9 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy, },
+    importProvidersFrom(IonicModule.forRoot({}),HttpClientModule),
     provideIonicAngular(),
     provideRouter(routes),
   ],
-});
+})
